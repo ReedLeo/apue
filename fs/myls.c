c9cd381934e7cc11dde872739232187408dcd568
leo <leo@localhost.localdomain>
2020-08-12 00:42:01 +0800

    parse sigle file

---
+++ fs/myls.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <glob.h>
+#include <limits.h>
+#include <pwd.h>
+#include <assert.h>
+
+
+#define OPT_STR "+ilR"
+
+#define TR_INODE    1
+#define TR_LONG     2
+#define TR_RECUR    4
+
+char* parse_file_mode(const mode_t st_mode)
+{
+    static char file_mode[12];
+    file_mode[0] = '-';
+    switch (st_mode & S_IFMT)
+    {
+    case S_IFSOCK:
+        file_mode[0] = 's';
+        break;
+    
+    case S_IFLNK:
+        file_mode[0] = 'l';
+        break;
+
+    case S_IFDIR:
+        file_mode[0] = 'd';
+        break;
+
+    default:
+        break;
+    }
+
+    file_mode[1] = (st_mode & S_IRUSR) ?  'r' : '-';
+    file_mode[2] = (st_mode & S_IWUSR) ?  'w' : '-';
+    file_mode[3] = (st_mode & S_ISUID) ?  's' : ((st_mode & S_IXUSR) ? 'x' : '-');
+
+    file_mode[4] = (st_mode & S_IRGRP) ?  'r' : '-';
+    file_mode[5] = (st_mode & S_IWGRP) ?  'w' : '-';
+    file_mode[6] = (st_mode & S_ISGID) ?  's' : ((st_mode & S_IXUSR) ? 'x' : '-');
+
+    file_mode[7] = (st_mode & S_IROTH) ?  'r' : '-';
+    file_mode[8] = (st_mode & S_IWOTH) ?  'w' : '-';
+    file_mode[9] = (st_mode & S_ISVTX) ?  't' : ((st_mode & S_IXUSR) ? 'x' : '-');
+    
+    file_mode[10] = '.';
+    file_mode[11] = '\0';
+
+    return file_mode;
+}
+
+/**
+ * Print file status accroding to the mode.
+*/
+void print_by_mode(const char* p_file_name, const struct stat* p_stbuf, const int mode)
+{
+    static char out_str_buf[PATH_MAX] = {0};
+    static char time_fmt_buf[PATH_MAX] = {0};
+    char* p_mode_str = NULL;
+    struct tm* p_st_tm = NULL;
+    size_t prefix_len = 0;
+    assert(p_stbuf);
+
+    out_str_buf[0] = '\0';
+    time_fmt_buf[0] = '\0';
+    if (mode & TR_INODE)
+    {
+        sprintf(out_str_buf, "%ld ", p_stbuf->st_ino);
+    }
+
+    prefix_len = strlen(out_str_buf);
+    if (mode & TR_LONG)
+    {
+        p_st_tm = localtime(&p_stbuf->st_mtim.tv_sec);
+        if (p_st_tm == NULL)
+        {
+            perror("local()");
+            exit(1);
+        }
+
+        strftime(time_fmt_buf, sizeof(time_fmt_buf), "%b %e %Y %H:%M", p_st_tm);
+        p_mode_str = parse_file_mode(p_stbuf->st_mode);
+        assert(p_mode_str);
+        sprintf(out_str_buf + prefix_len, "%s %ld %s %s %ld %s %s\n"
+            , p_mode_str
+            , p_stbuf->st_nlink
+            , getpwuid(p_stbuf->st_uid)->pw_name
+            , getpwuid(p_stbuf->st_gid)->pw_name
+            , p_stbuf->st_size
+            , time_fmt_buf      //"Replace by time format"
+            , p_file_name);
+    }
+    else
+    {
+        sprintf(out_str_buf + prefix_len, "%s ", p_file_name);
+    }
+    printf("%s", out_str_buf);
+}
+
+/**
+ * If recursivly mode is set, it will traverse by depth-first.
+ */
+void traverse(const char* path, const int mode)
+{
+    struct stat stbuf = {0};
+    const char* p_file_name = NULL;
+
+    if (lstat(path, &stbuf) < 0)
+    {
+        perror("stat()");
+        exit(1);
+    }
+
+    // is a normal file
+    if (S_ISDIR(stbuf.st_mode) == 0)
+    {
+        p_file_name = strrchr(path, '/');
+        if (p_file_name == NULL)
+            p_file_name = path;
+        print_by_mode(p_file_name, &stbuf, mode);
+        return;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int opt, tr_mode;
+    
+    tr_mode = 0;
+    while ((opt = getopt(argc, argv, OPT_STR)) >= 0)
+    {
+        switch (opt)
+        {
+        case 'i':
+            tr_mode |= TR_INODE;
+            puts("i");
+            break;
+
+        case 'l':
+            tr_mode |= TR_LONG;
+            break;
+
+        case 'R':
+            tr_mode |= TR_RECUR;
+            puts("R");
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    // The rest parameters of argv[] are all non-option parameters.
+    while (argv[optind])
+    {
+        traverse(argv[optind++], tr_mode);
+    }
+    return 0;
+}
