82fc6d0647f6cb60e7ad23e73ac6fdb703e998e2
leo <leo@localhost.localdomain>
2020-08-13 23:15:39 +0800

    scandir demo

---
+++ fs/scandir.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define FN_NAME "."
+
+int main(int argc, char** argv)
+{
+    int file_count = 0; 
+    struct dirent** name_list = NULL;
+
+    file_count = scandir(FN_NAME, &name_list, NULL, alphasort);
+    if (file_count < 0 || name_list == NULL)
+    {
+        perror("scandir()");
+        return 1;
+    }
+    printf("%d files in %s, there are:\n", file_count, FN_NAME);
+    while (file_count--)
+    {
+        printf("%s\n", name_list[file_count]->d_name);
+        free(name_list[file_count]);
+    }
+    free(name_list);
+    
+    return 0;
+}
