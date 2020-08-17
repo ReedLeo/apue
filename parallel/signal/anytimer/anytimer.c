e09747ac28fc6d2647028b20247cf11379a23aba
leo <hireedleo@gmail.com>
2020-08-17 21:20:36 +0800

    add some comments.

--- parallel/signal/anytimer/anytimer.c
+++ parallel/signal/anytimer/anytimer.c
@@ -47,6 +47,10 @@ static int get_free_pos(void)
     return -1;
 }
 
+
+/**
+ * Task schedule procedure, driven by alarm(1).
+*/
 static void task_sched(int signum)
 {
     tmtask_st* p_task = NULL;
@@ -61,6 +65,13 @@ static void task_sched(int signum)
             {
                p_task->handler(p_task->args);
                p_task->task_status = AT_ST_CANCEL;
+               /**
+                * !! A real-time signal dose not lose !!
+                * raise a self-defined real-time signal.
+                * because we've blocked this signal, it 
+                * will pend and until the at_wait_task()
+                * consume it.
+               */
                raise(SIGTSKFIN);
             }
         }
@@ -70,10 +81,13 @@ static void task_sched(int signum)
 
 static void module_unload(void)
 {   
+    /* recover signal mask and signal handlers to the orignal
+    *   status before loading this module.
+    */
     sigprocmask(SIG_SETMASK, &gs_saved_set, NULL);
     signal(SIGTSKFIN, gs_old_fintsk_handler);
     signal(SIGALRM, gs_old_alrm_handler);
-    alarm(0);
+    alarm(0);   // clean the alarm
     for (int i = 0; i < TASK_MAX_NUM; ++i)
     {
         free(gs_tasks_tbl[i]);
@@ -83,20 +97,41 @@ static void module_unload(void)
 
 static void module_load(void)
 {
+    /* 
+    *  When receiving a SIGTSKFIN real-time
+    *  signal, current thread dosen't need to have handler to 
+    *  process it or exit. Ignoring it will be fine.
+    */ 
     gs_old_fintsk_handler = signal(SIGTSKFIN, SIG_IGN);
-    sigemptyset(&gs_block_set);
-    sigaddset(&gs_block_set, SIGTSKFIN);
-    sigprocmask(SIG_UNBLOCK, &gs_block_set, &gs_saved_set);
-    if (sigprocmask(SIG_BLOCK, &gs_block_set, &gs_old_set) < 0)
+    sigemptyset(&gs_block_set); // init sigset to empty.
+    sigaddset(&gs_block_set, SIGTSKFIN);    // add our-self defined real-time signal.
+    /**
+     * If there already have blocked the real-time signal we defined, 
+     * we unblock it first and save the original sigset to gs_saved_set
+     * for recovering.
+    */
+    if ( (sigprocmask(SIG_UNBLOCK, &gs_block_set, &gs_saved_set) < 0)
+      || (sigprocmask(SIG_BLOCK, &gs_block_set, &gs_old_set) < 0)
+     )
     {
         perror("sigprocmask()");
         exit(EXIT_FAILURE);
     }
+    /**
+     * save original SIGALRM's handler and set a new one.
+    */
     gs_old_alrm_handler = signal(SIGALRM, task_sched);
     alarm(1);
     atexit(module_unload);
 }
 
+
+/**
+ * Remove a task identified by descriptor {int}td.
+ * Return value: {int}
+ *  td ,on success, is the removed task's descriptor.
+ *  -EINVAL, on falied, because the descriptor td is invalid.
+*/
 static int rm_task(const int td)
 {
     tmtask_st* ptsk = NULL;
@@ -108,6 +143,12 @@ static int rm_task(const int td)
     return td;
 }
 
+/**
+ * Initialize anytimer.
+ * Return value {int}:
+ *  0, on success;
+ *  -EAGAIN, already initilized.
+*/
 int at_init()
 {
     if (!gs_inited)
@@ -123,6 +164,19 @@ int at_init()
     
 }
 
+/**
+ * Add a anytimer task into task list. 
+ * Usually we use AT_ST_RUNNABLE as the status argument, after interval seconds late, 
+ * the task handler will be executed and its status will be changed to AT_ST_CANCEL.
+ * If status ot the task is not AT_ST_RUNNABLE, it will not count down for this task until
+ * its status being changed to AT_ST_RUNNABLE.
+ * 
+ * Return value:
+ *  non-negtive, on success, is the descriptor of the task we just added.
+ *  -EINVAL, some arguments are invalid.
+ *  -EXFULL, there isn't space in task descriptor table.
+ *  -ENOMEM, there isn't enough heap memory in system.
+*/
 int at_add_task(at_handler_t const handler, void* args, const int interval, at_status_enum status)
 {
     int td = -1;
@@ -158,8 +212,12 @@ int at_add_task(at_handler_t const handler, void* args, const int interval, at_s
 }
 
 /**
- *  0 -- disable
- *  non-zero -- enable 
+ * Pause the task indentifyed by td.
+ * 
+ * Return value:
+ *  0, on success.
+ *  -EINVAL, td is invalid.
+ *  -EINVAL, the task indentifyed by td alredy been canceled.
  */
 int at_pause_task(int td)
 {
@@ -179,6 +237,13 @@ int at_pause_task(int td)
     return 0;
 }
 
+/**
+ * Resume a task identifyed by td.
+ * 
+ * Return value:
+ *  0, on success.
+ *  -EINVAL, ts is invalid.
+*/
 int at_resume_task(int td)
 {
     tmtask_st* ptsk = NULL;
@@ -194,6 +259,15 @@ int at_resume_task(int td)
     return 0;
 }
 
+
+/**
+ * Cancel a task identifyed by td
+ * 
+ * Return value:
+ *  0, on success.
+ *  -EINVAL, td is invalid.
+ *  -EAGAIN, the task has already been canceled.
+ */
 int at_cancel_task(const int td)
 {
     tmtask_st* ptsk = NULL;
@@ -212,6 +286,15 @@ int at_cancel_task(const int td)
     return 0;
 }
 
+
+/**
+ * Wait a task to terminate. This function dosen't return until the 
+ * task is terminated.
+ * 
+ * Return value:
+ *  non-negtive, on success, is the td we waited for.
+ *  -EINVAL, td is invalid.
+*/
 int at_wait_task(const int td)
 {
     tmtask_st* ptsk = NULL;
@@ -229,6 +312,14 @@ int at_wait_task(const int td)
     return rm_task(td);
 }
 
+
+/*
+* Destory anytimer and release resources.
+*  
+* Return value:
+*  0, on success.
+*  -EAGAIN, already destoryed.
+*/
 int at_destory()
 {
     if (gs_inited)
