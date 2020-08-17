6939b3fce689a94abf903c206cbd430990d1d57f
leo <hireedleo@gmail.com>
2020-08-18 00:42:47 +0800

    use sigaction for normally exiting.

--- process/mydaemon.c
+++ process/mydaemon.c
@@ -12,7 +12,11 @@
 
 #include <errno.h>
 
+// for sigagction
+#include <signal.h>
+
 #define FNAME "/tmp/out"
+static FILE* gs_fp;
 
 int daemonize()
 {
@@ -47,19 +51,46 @@ int daemonize()
     return 0;
 }
 
+static void undaemonize(int signum, siginfo_t* si, void* args)
+{
+
+    fprintf(gs_fp, "Received a signal: [%d](%s), which is sent by %s"
+        , si->si_signo, strsignal(si->si_signo)
+        , (si->si_code == SI_KERNEL ? "Kernel" : "Others")
+        );
+    fflush(gs_fp);
+    // signal sent by the kernel.
+    if (si->si_code == SI_KERNEL)
+    {
+        fclose(gs_fp);
+        closelog();
+        exit(0);
+    }
+}
+
 int main()
 {
-    FILE* fp;
+    struct sigaction sa = {0};
+    
+    sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGINT);
+    sigaddset(&sa.sa_mask, SIGTERM);
+    sigaddset(&sa.sa_mask, SIGQUIT);
+    sa.sa_sigaction = undaemonize;
+    sa.sa_flags |= SA_SIGINFO;
+
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
 
     openlog("mydaemon", LOG_PID, LOG_DAEMON);
-
     if (daemonize())
     {
         syslog(LOG_ERR, "daemonize: %s", strerror(errno));
         exit(1);
     }
     
-    if ((fp = fopen(FNAME, "w")) == NULL)
+    if ((gs_fp = fopen(FNAME, "w")) == NULL)
     {
         syslog(LOG_ERR, "fopen: %s", strerror(errno));
         exit(1);
@@ -69,14 +100,11 @@ int main()
 
     for (int i = 0; ; ++i)
     {
-        fprintf(fp, "%d\n", i);
-        fflush(fp);
+        fprintf(gs_fp, "%d\n", i);
+        fflush(gs_fp);
         syslog(LOG_DEBUG, "%d is printed.", i);
         sleep(1);
     }
-
-    fclose(fp);
-    closelog();
 
     return 0;
 }
