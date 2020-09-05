b4a1924addd4a7b7d900439009b298f468428926
leo <hireedleo@gmail.com>
2020-09-05 17:53:57 +0800

    demo of sysV's message queue

--- ipc/xsi/msg/recmsg.c
+++ ipc/xsi/msg/recmsg.c
@@ -3,25 +3,64 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <signal.h>
 
 #include "proto.h"
+
+static int mq = 0 ;
+
+static void destroy(void)
+{
+	if (mq >= 0)
+		msgctl(mq, IPC_RMID, NULL);
+}
+
+static void sig_handle(int signum)
+{
+	destroy();
+	exit(signum);
+}
+typedef void (*sighandler_t)(int);
+
+static void init()
+{
+	atexit(destroy);
+	signal(SIGTERM, sig_handle);
+	signal(SIGINT, sig_handle);
+	signal(SIGQUIT, sig_handle);
+}
 
 int main(int argc, char** argv)
 {
 	key_t  key;
-	
+	struct msg_st msgbuf = {0};
+
+	init();
+
 	key = ftok(KEYPATH, KEYPROJ);
 	if (key == -1)
 	{
-		perror("ftok()")
+		perror("ftok()");
 		exit(1);
 	}
 
-	msgget(key, );
-
-	msgrcv();
-
-	msgctl();
+	mq = msgget(key, IPC_CREAT | 0600);
+	if (mq < 0)
+	{
+		perror("msgget()");
+		exit(1);
+	}
+	
+	while (1)
+	{
+		if (msgrcv(mq, &msgbuf, sizeof(msgbuf) - sizeof(msgbuf.mtype), 0, 0) < 0)
+		{
+			perror("msgrcv()");
+			exit(1);
+		}
+		printf("%s\nMath=%d\nChinese=%d\n", msgbuf.name, msgbuf.math, msgbuf.chinese);
+	}	
+	
 	
 	return 0;
 }
--- ipc/xsi/msg/sndmsg.c
+++ ipc/xsi/msg/sndmsg.c
@@ -3,17 +3,43 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <string.h>
 
 #include "proto.h"
 
 int main(int argc, char** argv)
 {
-	ftok();
+	key_t key;
+	int mqid = 0;
+	struct msg_st sndbuf = {0};
 
-	msgget();
+	key = ftok(KEYPATH, KEYPROJ);
+	if (key < 0)
+	{
+		perror("ftok()");
+		exit(1);
+	}
 
-	msgsnd();
+    mqid = msgget(key, 0);
+	if (mqid < 0)
+	{
+		perror("msgget()");
+		exit(1);
+	}
+	
+	sndbuf.mtype = 1;
+	strcpy(sndbuf.name, "Leo");
+	sndbuf.math = 98;
+	sndbuf.chinese = 90;
 
-	msgctl();
+	if (msgsnd(mqid, &sndbuf, sizeof(sndbuf) - sizeof(sndbuf.mtype), 0) < 0)
+	{
+		perror("msgsnd()");
+		exit(1);
+	}
+
+	//	It's receiver's responsiblity to remove
+	//	message queue.
+	//	msgctl();
 	return 0;
 }
